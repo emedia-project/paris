0d7df58b40ee366d702678a0b12bc73ab4d77165
glejeune <gregoire.lejeune@free.fr>
2014-04-13 13:27:25 +0200

    Add possibility to get IP and port from an external function

--- src/paris_app.erl
+++ src/paris_app.erl
@@ -7,17 +7,29 @@
 start(_StartType, [AppName]) ->
   Port = case application:get_env(AppName, port) of
     {ok, P} -> P;
-    _ -> 8080
+    _ -> 
+      case application:get_env(AppName, port_from) of
+        {Module, Function, Args} -> erlang:apply(Module, Function, Args);
+        _ -> 8080
+      end
   end,
   IP = case application:get_env(AppName, ip) of
     {ok, I} ->
       [A, B, C, D] = [list_to_integer(X) || X <- string:tokens(I, ".")],
       {A, B, C, D};
-    _ -> {0, 0, 0, 0}
+    _ -> 
+      case application:get_env(AppName, ip_from) of
+        {Module1, Function1, Args1} -> erlang:apply(Module1, Function1, Args1);
+        _ -> {0, 0, 0, 0}
+      end
   end,
   MaxConn = case application:get_env(AppName, max_conn) of
     {ok, MC} -> MC;
-    _ -> 100
+    _ -> 
+      case application:get_env(AppName, max_conn_from) of
+        {Module2, Function2, Args2} -> erlang:apply(Module2, Function2, Args2);
+        _ -> 100
+      end
   end,
   Routes      = paris_router:routes(AppName),
   Dispatch    = cowboy_router:compile(Routes),
