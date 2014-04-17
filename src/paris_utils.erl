7503041179c112be73a7259ceed54fc06616343d
glejeune <gregoire.lejeune@free.fr>
2014-04-17 14:55:56 +0200

    Remove unused function

--- src/paris_utils.erl
+++ src/paris_utils.erl
@@ -1,28 +1,12 @@
 -module(paris_utils).
 
 -export([
-  readlines/1,
   mime/1
 ]).
 
-readlines(FileName) ->
-  case file:open(FileName, [read]) of
-    {ok, Device} ->
-      try get_all_lines(Device)
-      after file:close(Device)
-      end;
-    _ -> error
-  end.
-
-get_all_lines(Device) ->
-  case io:get_line(Device, "") of
-    eof  -> [];
-    Line -> Line ++ get_all_lines(Device)
-  end.
-
 mime(File) ->
   [$.|Ext] = filename:extension(File),
-  case mimetypes:extension(Ext) of
+  case mimetypes:extension(string:to_lower(Ext)) of
     [M|_] -> M;
     T -> T
   end.
