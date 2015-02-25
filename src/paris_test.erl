fd8830edcd51baf9c78c63d75dca174ca97fd7b5
glejeune <gregoire.lejeune@free.fr>
2015-02-26 00:02:45 +0100

    Update test

--- src/paris_test.erl
+++ src/paris_test.erl
@@ -3,6 +3,8 @@
 %% @end
 -module(paris_test).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -export([
          start/1
         ]).
@@ -18,9 +20,18 @@
 % Assert
 -export([
          assert_ok/1,
+         assert_code/2,
+         assert_not_code/2,
+         assert_has_body/1,
+         assert_not_has_body/1,
          assert_redirect/1,
          assert_not_found/1,
          assert_header/3
+        ]).
+
+-export([
+         log/1,
+         log/2
         ]).
 
 -define(HTTP_OPTIONS, [{timeout, infinity},
@@ -39,30 +50,104 @@
 %% {sync, boolean()} | {stream, stream_to()} | {body_format, body_format()} | {full_result, boolean()} | {headers_as_is, boolean() | {socket_opts, socket_opts()} | {receiver, receiver()}, {ipv6_host_with_brackets, boolean()}}
 
 start(AppName) ->
+  hackney:start(),
   os:putenv("PARIS_RUN_MODE", "production"),
   AppName:start().
 
-request(Mode, Url, Fun) ->
-  request(Mode, Url, [], "", [], Fun).
+request(Method, URL, Fun) ->
+  request(Method, URL, [], <<>>, [], Fun).
 
-request(Mode, Url, Headers, Body, Options, Fun) ->
-  todo.
+request(Method, URL, Headers, Body, Options, Fun) when is_atom(Method),
+                                                       is_binary(URL),
+                                                       is_list(Headers),
+                                                       is_binary(Body),
+                                                       is_list(Options),
+                                                       is_function(Fun) ->
+  Fun(case hackney:request(Method, eutils:to_binary(URL), Headers, eutils:to_binary(Body), Options) of
+        {ok, StatusCode, RespHeaders, ClientRef} ->
+          case hackney:body(ClientRef) of
+            {ok, RespBody} ->
+              {ok, StatusCode, RespHeaders, RespBody};
+            E -> 
+              E
+          end;
+        {ok, StatusCode, RespHeaders} ->
+          {ok, StatusCode, RespHeaders, <<>>};
+        E ->
+          E
+      end).
 
-follow(LinkOrRedir, Assertions, Continuations) ->
-  todo.
+follow(Method, URL, Fun) ->
+  case http_uri:parse(eutils:to_string(URL)) of
+    {ok, {Scheme, UserInfo, Host, Port, _, _}} ->
+      Base = eutils:to_binary(
+               case UserInfo of
+                 [] -> eutils:to_string(Scheme) ++ "://" ++ 
+                       Host ++ ":" ++ eutils:to_string(Port);
+                 _ -> eutils:to_string(Scheme) ++ "://" ++ 
+                      UserInfo ++ "@" ++ 
+                      Host ++ ":" ++ eutils:to_string(Port)
+               end),
+      request(Method, URL, fun({ok, StatusCode, RespHeaders, <<>>}) when StatusCode >= 300, StatusCode < 400 ->
+                               case get_header(<<"Location">>, RespHeaders) of
+                                 <<>> -> Fun({error, not_redirect});
+                                 Location -> request(get, <<Base/binary, Location/binary>>, Fun)
+                               end;
+                              (X) ->
+                               Fun(X)
+                           end);
+    E ->
+      Fun(E)
+  end.
 
 submit(FormName, FormValue, Assertions, Continuations) ->
   todo.
 
-assert_ok(Response) ->
-  todo.
+assert_ok({ok, Code, _, _}) ->
+  ?assert(Code >= 200 andalso Code < 300);
+assert_ok(_) ->
+  ?assert(false).
 
-assert_redirect(Response) ->
-  todo.
+assert_code({ok, Code, _, _}, ExpectedCode) ->
+  ?assert(Code =:= ExpectedCode);
+assert_code(_, _) ->
+  ?assert(false).
 
-assert_not_found(Response) ->
-  todo.
+assert_not_code({ok, Code, _, _}, ExpectedCode) ->
+  ?assert(Code =/= ExpectedCode);
+assert_not_code(_, _) ->
+  ?assert(false).
 
-assert_header(Response, Header, Value) ->
-  todo.
+assert_has_body({ok, _, _, Body}) ->
+  ?assert(<<>> =/= Body);
+assert_has_body(_) ->
+  ?assert(false).
 
+assert_not_has_body({ok, _, _, Body}) ->
+  ?assert(<<>> =:= Body);
+assert_not_has_body(_) ->
+  ?assert(false).
+
+assert_redirect({ok, Code, _, _}) ->
+  ?assert(Code >= 300 andalso Code < 400);
+assert_redirect(_) ->
+  ?assert(false).
+
+assert_not_found({ok, 404, _, _}) ->
+  ?assert(true);
+assert_not_found(_) ->
+  ?assert(false).
+
+assert_header({ok, _, Headers, _}, Header, Value) ->
+  ?assert(get_header(Header, Headers) =:= eutils:to_binary(Value));
+assert_header(_, _, _) ->
+  ?assert(false).
+
+log(Fmt, Data) ->
+  ?debugFmt(Fmt, Data).
+
+log(Data) ->
+  ?debugMsg(Data).
+
+get_header(Header, Headers) ->
+  elists:keyfind(eutils:to_binary(Header), 1, Headers, <<>>).
